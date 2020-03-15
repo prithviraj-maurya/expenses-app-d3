a99487780a10cad597f87ffd1efd4a19bd92aa36
ramraj666 <pmaurya196@gmail.com>
2020-03-15 13:26:54 +0530

    Updates

---
+++ precache-manifest.58a8f83902ad2b1a603ee9c76474a3b1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c346a9428c07ce58b031da233e4f516e",
+    "url": "/react-expenses-app/index.html"
+  },
+  {
+    "revision": "c917c22d6a0c6d845076",
+    "url": "/react-expenses-app/static/css/main.c44fd510.chunk.css"
+  },
+  {
+    "revision": "d50f4a361c2a08265a9d",
+    "url": "/react-expenses-app/static/js/2.cefd8717.chunk.js"
+  },
+  {
+    "revision": "e471a239748f2097c480427663bdc7f0",
+    "url": "/react-expenses-app/static/js/2.cefd8717.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c917c22d6a0c6d845076",
+    "url": "/react-expenses-app/static/js/main.0bfb44bf.chunk.js"
+  },
+  {
+    "revision": "7f516043f0dfc16577a0",
+    "url": "/react-expenses-app/static/js/runtime-main.64b99e71.js"
+  },
+  {
+    "revision": "947af042f328b2f7732fcae021170599",
+    "url": "/react-expenses-app/static/media/CatMule-Caps.947af042.otf"
+  },
+  {
+    "revision": "e6d1a273c8000ee917d4b27c24282e6a",
+    "url": "/react-expenses-app/static/media/LibreBaskerville-Bold.e6d1a273.ttf"
+  },
+  {
+    "revision": "d32e21a107beb9e1c5cfa098733433ce",
+    "url": "/react-expenses-app/static/media/LibreBaskerville-Italic.d32e21a1.ttf"
+  },
+  {
+    "revision": "27b9efe7ae34478c82311efb90011607",
+    "url": "/react-expenses-app/static/media/LibreBaskerville-Regular.27b9efe7.ttf"
+  },
+  {
+    "revision": "b6937761491a737f6e556ff1109407e0",
+    "url": "/react-expenses-app/static/media/delete.b6937761.svg"
+  }
+]);
